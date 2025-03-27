d539a02894bc4bf30c9e3489263b29e0f601a736
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-28 00:32:12 +0100

    feat: implement font loading with splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,45 @@
 
 import './global.css'; // global styles (incl. Tailwind CSS definitions).
 
+import { useEffect } from 'react';
+
 import {
   Slot, // renders the currently selected route (similar to `children` in React).
   Stack,
+  SplashScreen,
 } from 'expo-router';
 
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
+
 const RootLayout = () => {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='index' />
-      <Stack.Screen name='profile' />
-    </Stack>
-  );
+  // load the font files asynchronously, https://docs.expo.dev/develop/user-interface/fonts/
+  const [fontsLoaded, error] = useFonts({
+    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
+  });
+
+  // font loading with splash screen.
+  useEffect(() => {
+    // if an error exists, then throw (raise) that error.
+    if (error) throw error;
+
+    // hide the native splash screen immediately (we must have content ready to display).
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [error, fontsLoaded]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }}></Stack>;
 };
 export default RootLayout;
