c667e86ebe5fc14b600036710ffca104af536bbf
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-27 13:14:35 +0100

    feat: install NativeWind and configure Tailwind CSS

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+// Metro Bundler is the default JavaScript bundler for React Native projects.
+// It takes our code and its dependencies and packages them into a single JavaScript file (or multiple bundles) that can be run on a mobile device or in a web browser.
+
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: './app/global.css' });
