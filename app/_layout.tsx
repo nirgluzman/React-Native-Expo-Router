c494db72f995ae53d40ef23165234ab0c298398e
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-27 11:59:27 +0100

    feat: define root layout and top-level route files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Root layout - share UI between multiple routes
+// https://docs.expo.dev/develop/file-based-routing/#root-layout
 
-export default function App() {
+import { Text } from 'react-native';
+
+import {
+  Slot, // renders the currently selected route (similar to `children` in React).
+} from 'expo-router';
+
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Text>Header</Text>
+      <Slot />
+      <Text>Footer</Text>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default RootLayout;
