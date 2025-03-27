0e60d9416bf0801bb801c0700cb5ab60882af3b6
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-27 22:25:02 +0100

    style: add global css import to root layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 // Root layout - share UI between multiple routes.
 // https://docs.expo.dev/develop/file-based-routing/#root-layout
 
+import './global.css'; // global styles (incl. Tailwind CSS definitions).
+
 import {
   Slot, // renders the currently selected route (similar to `children` in React).
   Stack,
