54c271564231b5ce50806f3703166d83e89d26f6
Nir Gluzman <nir.gluzman@gmail.com>
2025-04-01 11:38:01 +0200

    feat: expand color definitions and export default colors object

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,16 +1,25 @@
-export const colors = {
+export default {
   primary: '#161622',
   secondary: {
     DEFAULT: '#FF9C01',
     100: '#FF9001',
     200: '#FF8E01',
+    300: '#FF7E01',
+    400: '#FF6E01',
+    500: '#FF5E01',
+    600: '#FF4E01',
+    700: '#FF3E01',
   },
   black: {
     DEFAULT: '#000',
     100: '#1E1E2D',
     200: '#232533',
+    300: '#1A1A24',
+    400: '#14151B',
+    500: '#0D0D13',
   },
   gray: {
     100: '#CDCDE0',
+    200: '#7b7b8b',
   },
 };
