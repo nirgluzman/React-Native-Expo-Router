14b2c1b9a87d47ea103a6ed02a5ced6b30e24e93
Nir Gluzman <nir.gluzman@gmail.com>
2025-04-09 03:52:01 +0200

    feat: integrate global error context for authentication

--- services/auth/auth.context.tsx
+++ services/auth/auth.context.tsx
@@ -7,6 +7,9 @@ import { useState, useEffect, createContext, type ReactNode } from 'react';
 import { type User as FirebaseUser } from 'firebase/auth';
 import { FirebaseError } from '@firebase/util'; // subclass of the standard JavaScript Error object. In addition to a message string and stack trace, it contains a string code.
 
+// imports the custom hook for accessing the global error management context.
+import { useErrorContext } from '../error/error.context';
+
 import { listenAuthState, signInRequest, signUpRequest, logoutRequest } from './auth.service';
 
 // define the context value type.
@@ -20,37 +23,33 @@ type AuthContextType = {
   isLoading: boolean; // tracking authentication state changes.
 };
 
-// create context with proper typing and default value.
+// create context with proper typing and default values.
 export const AuthContext = createContext<AuthContextType>({
   user: null,
   isAuthenticated: false,
   onSignIn: async () => {
-    throw new Error('Cannot use onSignIn without AuthProvider');
+    // using throw new Error(...) within the default values of a createContext object serves as a safeguard
+    // against using context functions without their required Provider higher in the component tree.
+    throw new Error('Cannot use onSignIn without AuthContextProvider');
   },
   onSignUp: async () => {
-    throw new Error('Cannot use onSignUp without AuthProvider');
+    throw new Error('Cannot use onSignUp without AuthContextProvider');
   },
   onLogout: async () => {
-    throw new Error('Cannot use onLogout without AuthProvider');
+    throw new Error('Cannot use onLogout without AuthContextProvider');
   },
   idToken: null,
   isLoading: false,
 });
-
-// format a Firebase Authentication error message.
-// https://firebase.google.com/docs/reference/js/auth#autherrorcodes (AuthErrorCodes)
-function formatAuthErrorMessage(errorMessage: string) {
-  return errorMessage
-    .replace('auth/', '')
-    .replace(/-/g, ' ')
-    .replace(/^./, (char: string) => char.toUpperCase());
-}
 
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<FirebaseUser | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [idToken, setIdToken] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // obtain the function responsible for updating the global error state.
+  const { handleError } = useErrorContext();
 
   const onSignIn = async (email: string, password: string) => {
     setIsLoading(true);
@@ -60,11 +59,8 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
       setIdToken(await currentUser.getIdToken());
       return { success: true, message: 'Sign in was successful' };
     } catch (err) {
-      if (err instanceof FirebaseError) {
-        return { success: false, message: formatAuthErrorMessage(err.code) };
-      } else {
-        return { success: false, message: 'An unexpected error occurred' };
-      }
+      handleError(err);
+      return { success: false, message: (err as FirebaseError).message };
     } finally {
       setIsLoading(false);
     }
@@ -78,11 +74,8 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
       setIdToken(await currentUser.getIdToken());
       return { success: true, message: 'Sign up was successful' };
     } catch (err) {
-      if (err instanceof FirebaseError) {
-        return { success: false, message: formatAuthErrorMessage(err.code) };
-      } else {
-        return { success: false, message: 'An unexpected error occurred' };
-      }
+      handleError(err);
+      return { success: false, message: (err as FirebaseError).message };
     } finally {
       setIsLoading(false);
     }
@@ -94,13 +87,10 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
       await logoutRequest();
       setUser(null);
       setIdToken(null);
-      return { success: true };
+      return { success: true, message: 'Logout was successful' };
     } catch (err) {
-      if (err instanceof FirebaseError) {
-        return { success: false, message: err.message };
-      } else {
-        return { success: false, message: 'An unexpected error occurred' };
-      }
+      handleError(err);
+      return { success: false, message: (err as FirebaseError).message };
     } finally {
       setIsLoading(false);
     }
