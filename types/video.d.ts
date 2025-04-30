8a732eac6dc68163ca4bfabe9a66c06a59de4996
Nir Gluzman <nir.gluzman@gmail.com>
2025-04-30 16:58:55 +0200

    refactor: change Video thumbnail type to ImageSourcePropType

--- types/video.d.ts
+++ types/video.d.ts
@@ -1,10 +1,13 @@
 //
 // Data structure representing the core attributes of a video, as stored in Firestore, but without the document identifier.
 //
+
+import { ImageSourcePropType } from 'react-native';
+
 type Video = {
   creator: { username: string; photoURL: string }; // the user who created the video.
   title: string; // the title of the video.
-  thumbnail: string; // URL or path to the video's thumbnail image.
+  thumbnail: ImageSourcePropType; // URL or path to the video's thumbnail image.
   video: string; // URL or path to the video file.
   prompt: string; // text prompt (GenAI) associated with the video.
   timestamp: Date;
@@ -13,6 +16,7 @@ type Video = {
 //
 // Data structure representing a Video document retrieved from Firestore, including its unique document identifier.
 //
+
 type VideoWithId = Video & { id: string }; // extends Video with the Firestore document's identifier.
 
 export type { Video, VideoWithId };
