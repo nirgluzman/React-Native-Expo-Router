adb05caf30a0d8f4c9d54044242e86b0c5d278f2
Nir Gluzman <nir.gluzman@gmail.com>
2025-05-08 08:55:29 +0200

    feat: upgrade Expo SDK to v53

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,10 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Firebase JS SDK for Auth doesn't work with Expo v53
+// https://docs.expo.dev/guides/using-firebase/?redirected#configure-metro
+config.resolver.sourceExts.push('cjs');
+config.resolver.unstable_enablePackageExports = false;
+
+// https://www.nativewind.dev/getting-started/installation#4-modify-your-metroconfigjs
 module.exports = withNativeWind(config, { input: './app/global.css' });
